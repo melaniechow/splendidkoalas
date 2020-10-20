96a643f4cf4fd6dc52d184b55a952a304d20654c
Celine Kim <celinekim@Celines-MacBook-Pro.local>
2020-10-20 01:20:03 -0700

    Selection Menu generator and soem backend TODOs:

--- script.js
+++ script.js
@@ -1,37 +1,136 @@
 /**
  * TODO
  * 
- * Listen to user inputs and determine when to create selection menu
- * Get location of user cursor
- * Selection menu generator
- * Move selection menu as user types
- * ?? Dynamic selection menu changes as user writes more?
+ * Reference: https://codepen.io/shotastage/pen/KaKwya
  * 
- * Replace text with suggestion after a trigger action
- *    Tab
- *    Enter
- * Remove menu 
+ * 
+ * Listen to user inputs and determine when to create selection menu []
+ * Get location of user cursor []
+ * Selection menu generator [DONE]
+ * Select option with arrow keys or on click []
+ * Move selection menu as user types []
+ * ?? Dynamic selection menu changes as user writes more? []
+ * 
+ * Replace text with suggestion after a trigger action []
+ *    Tab []
+ *    Enter []
+ * Remove menu  []
  * 
  */
 
-var textAreaID = "input";
-var firstOptionId = "option1";
+var textAreaID = "Input";
+var firstOptionId = "Option1";
+var optionClassName = "Option"
+var selectionClassName = "Selection";
 
-function addLineNumbers() {
-  var content = document.getElementById(textAreaID).innerHTML.split("\n");
-  var newContent = [];
-  for (var i = 0; i < content.length; i++) { 
-    newContent.push((i + 1) + "  " + content[i] + "\n");
-  } 
-  document.getElementById(textAreaID).innerHTML = newContent.join("");
+var textarea = document.getElementById(textAreaID);
+
+var selectionMenuPresent = true; 
+var currentOptionId = firstOptionId ; // Highlighted Option
+
+
+
+
+/*
+ * Detects when selection Menu should be generated and where
+ */
+// TODO:
+
+
+/*
+ * Detects when selection Menu should be moved and where
+ */
+// TODO:
+
+
+/* 
+ * Create selection menu
+ * Inputs:
+ *    words - array or words in selection menu
+ *    x, y - absolute position of menu
+ */
+function createSelectionMenu(words, left, top){
+  var newDiv = document.createElement("div");
+  newDiv.className = selectionClassName;
+
+  var ul = document.createElement("ul");
+  for (var i = 0; i<words.length; i++) {
+    var option = document.createElement("li");
+    option.className = optionClassName;
+    option.id = optionClassName + (i+1);
+    option.innerHTML = words[i];
+    option.onclick = cursorClick();
+    ul.appendChild(option)
+  }
+  newDiv.appendChild(ul);
+  document.body.appendChild(newDiv);
+  newDiv.style.position = "absolute";
+  newDiv.style.left = left + "px";
+  newDiv.style.top = top + "px";
+  highlightFirstOption();
 }
 
-/* Create selection menu based on user input: text */
-function createSelectionMenu(text){
+/* 
+ * Deletes the selection menu
+ * Removes div object with selection class name
+ */
+function deleteSelectionMenu() {
+  // TODO:
+  selectionMenuPresent = false;
   return
 }
 
-/* Highlight the first option in the selection menu */
+/* 
+ * Move selection menu to left top position
+ */
+function moveSelectionMenu(left, top) {
+  // TODO:
+  return
+}
+
+/*
+ * Replace the text[start:end] with newText, and returns
+ * text with the replacement
+ * Inputs:
+ *    start - start of substring to replace
+ *    end - end of substring to replace
+ *    text - text to modify
+ *    newText - text to replace with
+ */
+function replaceText(start, end, text, newText) {
+  // TODO: 
+  return;
+}
+
+/*
+ * cursorClick is called when an option is selected with a cursor 
+ * Replaces word with suggestion 
+ */
+function cursorClick() {}
+
+/*
+ * tabClick is called when an option is selected with a tab
+ * Replaces word with suggestion 
+ */
+function tabPress() {}
+
+/*
+ * enterClick is called when an option is selected with a tab
+ * Replaces word with suggestion 
+ */
+function enterPress() {}
+
+/*
+ * Moves Highlighted option based on arrow key
+ * Inputs:
+ *  up - boolean if arrowPress was up
+ */
+function arrowPress(up) {}
+
+
+/* 
+ * Highlight the first option in the selection menu 
+ */
 function highlightFirstOption() {
   var option = document.getElementById(firstOptionId);
   option.style.backgroundColor =  "#353A55";
@@ -41,14 +140,15 @@ function highlightFirstOption() {
  * Functions to call at start of program
  */
 function setUp() {
-  highlightFirstOption();
+  createSelectionMenu(["Peaches", "Totoro"],450,850);
 }
 
 // Keypress event listenter
 document.body.onkeypress = function(e){
   if(e.code == "Enter"){
-  }
+  } 
   // Add other cases down here
 }
+
 
 setUp();
